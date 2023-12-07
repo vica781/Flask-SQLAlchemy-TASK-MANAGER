f5efebd1b7083b311ebb1e3b85d27989ee4ea9fe
Victoria Kalenik-Harris <vk781@hotmail.com>
2023-12-07 19:52:20 +0000

    Add navbar and footer

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
+
+  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+  // var collapsibleElem = document.querySelector('.collapsible');
