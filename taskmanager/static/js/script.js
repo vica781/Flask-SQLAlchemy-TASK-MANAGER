444608b337abdd4bd09e5f2a09438dcb0a0d3c48
Victoria Kalenik-Harris <vk781@hotmail.com>
2023-12-07 20:47:56 +0000

    Replace render_template to tasks.html

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,8 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
+document.addEventListener("DOMContentLoaded", function() {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
+});
