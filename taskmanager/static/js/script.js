f116616d038cdabc932788f21a39f1d5ed78f147
Victoria Kalenik-Harris <vk781@hotmail.com>
2023-12-13 20:30:26 +0000

    Add tasks viewing functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,30 +2,23 @@ document.addEventListener("DOMContentLoaded", function() {
   // sidenav initialization
   let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
+
+  // modal initialization
+  let modals = document.querySelectorAll(".modal");
+  M.Modal.init(modals);
+  
+  // datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+  });
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+
+  // collapsible initializataion
+  let collapsibles = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(collapsibles);
 });
-
-// Modal initialization
-let modal = document.querySelectorAll(".modal");
-M.Modal.init(modal);
-
-// Datepicker initialization
-let datepicker = document.querySelectorAll(".datepicker");
-M.Datepicker.init(datepicker, {
-  format: "dd mmmm, yyyy",
-  yearRange: 3,
-  i18n: {done: "Select"}
-});
-
-// Select initialization
-let selects = document.querySelectorAll("select");
-M.FormSelect.init(selects);
-
-
-
-
-
-
-
-
-
-
